77937e27f0af1cc9c48df3287969f9f2b67e7527
mustafa <mustafa.ahmad@3lmdigital.com>
2020-03-11 09:57:41 +0200

    Updates

---
+++ precache-manifest.dba11a9f1bac0beb5e7bae81f949880e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109e4ab2f456484ab2feabf1b4810633",
+    "url": "/music-search/index.html"
+  },
+  {
+    "revision": "b720e0b8f6dd10dfbb29",
+    "url": "/music-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56fc466b63b9b82d9874",
+    "url": "/music-search/static/js/2.fa047e6f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/music-search/static/js/2.fa047e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b720e0b8f6dd10dfbb29",
+    "url": "/music-search/static/js/main.52276b83.chunk.js"
+  },
+  {
+    "revision": "ad9ab6078c9a80321ef9",
+    "url": "/music-search/static/js/runtime-main.6cba4259.js"
+  }
+]);
